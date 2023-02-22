5040ca59636ef8547530094a8eefa9a830370445
Cuauhtemoc <cuauh@soundscouts.com>
2023-02-22 18:53:09 +1100

    -hardcoded PoC logic and structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,91 +1,91 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import React, {useState} from 'react';
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    const [questionOpen, setQuestionOpen] = useState(false)
+    const [questionOpen2, setQuestionOpen2] = useState(false)
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+    return (
+        <div className="">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+            <main className="w-1/2  justify-center text-left m-auto">
+                <h1 className="text-4xl font-bold my-5 w-full text-center">
+                    Interview with Cuauh "oddgoo" Moreno
+                </h1>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+                <div className="space-y-5 ">
+                    <div className="bg-slate-100 rounded-xl p-4 dark:bg-slate-800 text-white w-full text-center font-bold"
+                         onClick={() => {
+                             setQuestionOpen(!questionOpen)
+                         }}>What was the inspiration for the game's concept and story?
+                    </div>
+                    {questionOpen &&
+                    <div className="space-y-5">
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+                        <p>The original Amberial (2007) has a curious story.</p>
+                        <p>I wanted to make a web game about breaking the 4th wall, literally.</p>
+                        <p>And as a web game contained in a window, I envisioned a simple marble character literally
+                            pushing against the walls of the browser,
+                            trying to use momentum, springs and strings to get enough force to break out.</p>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                        <img className="m-auto shadow-xl shadow-gray-600 mb-10 rounded-2xl" width={380} alt={""}
+                            src={"https://picon.ngfiles.com/378000/flash_378790_card.webp?f1675351127"}/>
+
+                        <p>So I started making that, but before I got to coding the actual wall breaking mechanics, I
+                            realised the game was fun enough as it was, and my focus shifter to building environments
+                            and soundscapes to create a proper feeling of adventure and traversal.
+                        </p>
+                        <p>The concept itself is heavily inspired by games like Marble Madness, Within a Deep Forest and
+                            Super Monkey Ball, where you cannot jump and all the focus on the environment and level
+                            design. In games like these, you are not only seeing where you can land and what obstacles
+                            to avoid, but you get to savour the angle of each bounce, the momentum against every gap.
+                        </p>
+                        <p>After the first Amberial, each new version has been an evolution of the concept heavily
+                            guided by player feedback. This all culminates in Amberial Dreams, which has everything each
+                            previous game had.
+                        </p>
+                        <p>As for the story, there has always been some environmental narrative in the background, but
+                            Amberial Dreams takes a huge leap and presents a story that evolves from and complements the
+                            game concepts and environments, giving life to abstract ideas.
+                        </p>
+                    </div>}
+                </div>
+
+                <div className="space-y-5 my-8">
+                    <div className="bg-slate-100 rounded-xl p-4 dark:bg-slate-800 text-white w-full text-center font-bold"
+                         onClick={() => {
+                             setQuestionOpen2(!questionOpen2)
+                         }}>How did you come up with the game's unique mechanics and features?
+                    </div>
+                    {questionOpen2 &&
+                        <div className="space-y-5">
+
+                            <p>Amberial is our life-long project to take everything that we like about platformers, and make an evolving universe that's all about creativity and the pure joy of rolling and bouncing around.
+                                Where there is a lack of jumping, your avatar makes up on speed and bouncing around. This places a unique focus on the level design; we want players to develop an intimate relation with its physics, the geometry and all the mechanisms, creating a strong sense of place.
+                            </p>
+                        </div>}
+                </div>
+
+            </main>
+
+            <footer className="flex h-24 w-full items-center justify-center border-t">
+                <a
+                    className="flex items-center justify-center gap-2"
+                    href="https://www.oddgoo.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Interview coded by www.oddgoo.com
+                </a>
+            </footer>
         </div>
-        <div>
-          <h3 className="text-2xl font-bold">Interview</h3>
-          <div className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 text-white">Question</div>
-          <div>Answer</div>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
 
 export default Home
